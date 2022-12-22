a30a095c8720dccfa7e8c1a2b2c459a3da16f646
jneda <julien.dane@gmail.com>
2022-12-22 18:23:51 +0100

    feat: restyle nav brand

--- view/partials/header.php
+++ view/partials/header.php
@@ -12,21 +12,23 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg bg-light sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">Bonjour tout le monde ! 🥸</a>
+      <a class="navbar-brand" href="index.php">
+        <span class="mb-0 h2">Bonjour tout le monde ! 🥸</span>
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link" href="login.php">Se connecter</a>
           </li>
-          <li class="nav-item">
+         <!--  <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item dropdown">
@@ -44,12 +46,12 @@
           </li>
           <li class="nav-item">
             <a class="nav-link disabled">Disabled</a>
-          </li>
+          </li> -->
         </ul>
-        <form class="d-flex" role="search">
+        <!-- <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </form> -->
       </div>
     </div>
   </nav>
