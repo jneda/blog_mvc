544c201941153eaea36d219b8f7bb4307087fce4
jneda <julien.dane@gmail.com>
2022-12-22 15:24:42 +0100

    feat: basic blog post controller

--- blogpost.php
+++ blogpost.php
@@ -1,5 +1,22 @@
 <?php
 
-$message = "Bienvenue sur cet article de blog !";
+require_once './lib/validation.php';
+require_once './model/manager/PostManager.php';
+
+$errorMessage = null;
+$post = null;
+
+if (empty($_GET)) {
+  $errorMessage = 'Adresse invalide !';
+} else {
+  $postId = sanitizeInput($_GET['id']);
+
+  if (!isInputValid($postId)) {
+    $errorMessage = 'Adresse invalide !';
+  } else {
+    $post = PostManager::getPostById($postId);
+  }
+}
+
 
 require_once 'view/blogpostView.php';
