9f21124cad7663815e86d57cc7a421cf727bf11d
jneda <julien.dane@gmail.com>
2022-12-22 15:24:11 +0100

    feat: add getPostById method

--- model/manager/PostManager.php
+++ model/manager/PostManager.php
@@ -13,4 +13,16 @@ class PostManager
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_CLASS, 'Post');
   }
+
+  public static function getPostById(int $idPost): Post
+  {
+    $databaseHandle = dbConnect();
+    $query = 'SELECT * FROM post WHERE id_post=:idPost';
+    $statement = $databaseHandle->prepare($query);
+    $statement->execute([
+      'idPost' => $idPost
+    ]);
+    $statement->setFetchMode(PDO::FETCH_CLASS, 'Post');
+    return $statement->fetch();
+  }
 }
