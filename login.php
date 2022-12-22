35d96124c256a1c781b97f0db092ea78abbe02fa
jneda <julien.dane@gmail.com>
2022-12-22 10:16:29 +0100

    feat: add login form

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'model/DBConnect.php';
+
+// $message = "Bienvenue sur cet article de blog !";
+
+require_once 'view/loginView.php';
---
+++ view/loginView.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'partials/header.php';
+
+?>
+
+<form class="container my-3">
+  <!-- <div class="mb-3">
+    <label for="username" class="form-label">Ton pseudo :</label>
+    <input type="text" class="form-control" id="username" name="username">
+  </div> -->
+  <div class="mb-3 col-md-6 offset-md-3">
+    <label for="email" class="form-label">Ton email :</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="mb-3 col-md-6 offset-md-3">
+    <label for="password" class="form-label">Ton mot de passe :</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary offset-md-3">Se connecter</button>
+</form>
+
+<?php
+
+require_once "partials/footer.php";
