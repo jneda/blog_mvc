b30666694c43736cbd55322565fd8fd3535e1fe6
jneda <julien.dane@gmail.com>
2022-12-22 18:24:22 +0100

    feat: style blog post page and home page

--- view/partials/blogPost.php
+++ view/partials/blogPost.php
@@ -1,8 +1,10 @@
-<div class="card blog-post col-md-6 offset-md-3">
-  <img src="<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>">
-  <div class="card-body">
-    <h5 class="card-title"><?php echo $post->getTitle() ?></h5>
-    <p class="card-text"><?php echo $post->getContent() ?></p>
+<div class="container my-3">
+  <div class="blog-post col-md-6 offset-md-3">
+    <img src="<?php echo $post->getImage() ?>" class="blog-post-img img-fluid rounded" alt="<?php echo $post->getTitle() ?>">
+    <div class="blog-post-body my-3">
+      <h1 class="blog-post-title fs-2"><?php echo $post->getTitle() ?></h5>
+      <p class="blog-post-text fs-5"><?php echo $post->getContent() ?></p>
+    </div>
     <a href="index.php" class="btn btn-primary">Je retourne lire d'autres articles !</a>
   </div>
 </div>
--- view/partials/blogPostsList.php
+++ view/partials/blogPostsList.php
@@ -1,5 +1,7 @@
+<div class="container my-3">
 <?php
-
 foreach ($posts as $post) {
   require 'blogpostCard.php';
 }
+?>
+</div>
