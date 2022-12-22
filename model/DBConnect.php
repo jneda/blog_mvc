a77881e60c5d3f4e359af16401038ac58979cb97
jneda <julien.dane@gmail.com>
2022-12-22 11:38:21 +0100

    fix: make a database connexion function

--- model/DBConnect.php
+++ model/DBConnect.php
@@ -1,8 +1,12 @@
 <?php
 
-try {
-  $databaseHandle = new PDO('mysql:host=localhost;dbname=tp_blog', 'root', '');
-  $databaseHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  die('Erreur : ' . $e->getMessage());
+function dbConnect(): PDO
+{
+  try {
+    $databaseHandle = new PDO('mysql:host=localhost;dbname=tp_blog', 'root', '');
+    $databaseHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $databaseHandle;
+  } catch (PDOException $e) {
+    die('Erreur : ' . $e->getMessage());
+  }
 }
