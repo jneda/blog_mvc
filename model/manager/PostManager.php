72c8564910bff374a532f375c730f5eccdca5156
jneda <julien.dane@gmail.com>
2022-12-22 11:37:48 +0100

    feat: add PostManager

---
+++ model/manager/PostManager.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'model//DBConnect.php';
+require_once 'model/classes/Post.php';
+
+class PostManager
+{
+  public static function fetchAllPosts(): array
+  {
+    $databaseHandle = dbConnect();
+    $query = 'SELECT * FROM post';
+    $statement = $databaseHandle->prepare($query);
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Post');
+  }
+}
