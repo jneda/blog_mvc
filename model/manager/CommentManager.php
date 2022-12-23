20010bd3d856fc9f5bf8d389e29c565d7517d4eb
jneda <julien.dane@gmail.com>
2022-12-23 18:21:55 +0100

    feat: add createComment method

--- model/manager/CommentManager.php
+++ model/manager/CommentManager.php
@@ -5,6 +5,10 @@ require_once './model/classes/Comment.php';
 
 class CommentManager
 {
+  /**
+   * Read static methods
+   */
+
   public static function fetchCommentsByPostId($postId): array|bool
   {
     $databaseHandle = dbConnect();
@@ -14,5 +18,25 @@ class CommentManager
       'postId' => $postId
     ]);
     return $statement->fetchAll(PDO::FETCH_CLASS, 'Comment');
+  }
+
+  /**
+   * Create static methods
+   */
+
+  public static function createComment(array $commentData): bool
+  {
+    $databaseHandle = dbConnect();
+    $query = '
+      INSERT INTO comment (id_post, id_author, date, content)
+      VALUES (:idPost, :idAuthor, :date, :content)
+    ';
+    $statement = $databaseHandle->prepare($query);
+    return $statement->execute([
+      'idPost' => $commentData['idPost'],
+      'idAuthor' => $commentData['idAuthor'],
+      'date' => (new DateTime())->format('Y-m-d H:i:s'),
+      'content' => $commentData['content']
+    ]);
   }
 }
