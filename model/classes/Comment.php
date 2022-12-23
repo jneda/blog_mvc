82b93c47c3841cc5d34609822662b8e49a4558bf
jneda <julien.dane@gmail.com>
2022-12-23 16:26:44 +0100

    feat: add Comment class

---
+++ model/classes/Comment.php
@@ -0,0 +1,39 @@
+<?php
+
+class Comment
+{
+  private int $id_comment;
+  private int $id_post;
+  private int $id_author;
+  private string $date;
+  private string $content;
+
+  /**
+   * Getters
+   */
+
+  public function getCommentId(): int
+  {
+    return $this->id_comment;
+  }
+
+  public function getPostId(): int
+  {
+    return $this->id_post;
+  }
+
+  public function getAuthorId(): int
+  {
+    return $this->id_author;
+  }
+
+  public function getDate(): string
+  {
+    return $this->date;
+  }
+
+  public function getContent(): string
+  {
+    return $this->content;
+  }
+}
