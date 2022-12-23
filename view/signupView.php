2515f4bc3a260f533f1eea495e388c4de915b0d4
jneda <julien.dane@gmail.com>
2022-12-23 09:30:05 +0100

    feat: add signup view

---
+++ view/signupView.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'partials/header.php';
+
+?>
+
+<form class="container my-3" action="signup.php" method="POST">
+  <div class="mb-3 col-md-6 offset-md-3">
+    <label for="username" class="form-label">Ton pseudo :</label>
+    <input type="text" class="form-control" id="username" name="username">
+  </div>
+  <div class="mb-3 col-md-6 offset-md-3">
+    <label for="email" class="form-label">Ton email :</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="mb-3 col-md-6 offset-md-3">
+    <label for="password" class="form-label">Ton mot de passe :</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary offset-md-3">S'inscrire</button>
+  <!-- <a href="signup.php" class="btn btn-outline-primary">Se connecter</a> -->
+</form>
+
+<?php
+
+require_once "partials/footer.php";
