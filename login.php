4007e732294556e71891648ce7a7c0fa1c88ab18
jneda <julien.dane@gmail.com>
2022-12-23 09:39:01 +0100

    feat: verify password vs hash

--- login.php
+++ login.php
@@ -1,12 +1,11 @@
 <?php
 
 require_once './model/manager/UserManager.php';
-
-// $message = "Bienvenue sur cet article de blog !";
-
 require_once './view/loginView.php';
 
 if (!empty($_POST)) {
+  // validate inputs
+
   require_once './lib/validation.php';
   $email = sanitizeInput($_POST['email']);
   $password = sanitizeInput($_POST['password']);
@@ -17,10 +16,19 @@ if (!empty($_POST)) {
     die();
   }
 
+  // fetch user
+
   $user = UserManager::getUserByEmail($email);
 
   if (!$user) {
     $errorMessage = 'Ton email est inconnu ! 🤔';
+    require_once './view/partials/errorAlert.php';
+    die();
+  }
+
+  // check password
+  if (!password_verify($password, $user->getPassword())) {
+    $errorMessage = 'Mot de passe erroné ! 🤔';
     require_once './view/partials/errorAlert.php';
     die();
   }
