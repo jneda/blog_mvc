93ef045ccd0faeacc3867458e0a13a7b0d9cfd5f
jneda <julien.dane@gmail.com>
2022-12-23 11:02:18 +0100

    fix: add margins to cards

--- view/partials/blogpostCard.php
+++ view/partials/blogpostCard.php
@@ -1,4 +1,4 @@
-<div class="card blog-post col-md-6 offset-md-3">
+<div class="card blog-post col-md-6 offset-md-3 my-3">
   <!-- <img src="<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>"> -->
   <div class="card-body">
     <h5 class="card-title"><?php echo $post->getTitle() ?></h5>
