3c6e828adff1daccc87a97da19412e4efebf77f3
jneda <julien.dane@gmail.com>
2022-12-24 19:05:56 +0100

    fix: tweak comment styles

--- view/partials/commentCard.php
+++ view/partials/commentCard.php
@@ -1,10 +1,14 @@
-<div class="card comment my-3">
-  <!-- <img src="<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>"> -->
-  <div class="card-body">
-    <p class="comment-author">
-      par <strong><?= $commentAuthor->getUsername() ?></strong> 
-      <span class="text-muted">- <?= $comment->getFormattedDate() ?></span>
-    </p>
-    <p class="card-text"><?php echo $comment->getContent() ?></p>
+<div class="row my-3">
+  <div class="col">
+    <div class="card">
+      <!-- <img src="<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>"> -->
+      <div class="card-body">
+        <p class="comment-author">
+          par <strong><?= $commentAuthor->getUsername() ?></strong>
+          <span class="text-muted">- <?= $comment->getFormattedDate() ?></span>
+        </p>
+        <p class="card-text"><?php echo $comment->getContent() ?></p>
+      </div>
+    </div>
   </div>
 </div>
--- view/partials/commentsList.php
+++ view/partials/commentsList.php
@@ -1,8 +1,6 @@
-<div class="my-3 col-md-6 offset-md-3">
   <?php
   foreach ($commentsData as $commentData) {
     ['comment' => $comment, 'author' => $commentAuthor] = $commentData;
     require './view/partials/commentCard.php';
   }
   ?>
-</div>
