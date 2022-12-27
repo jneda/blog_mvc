a42d441e7e582d8e9b5ed8386cdf4eded2eb8a5c
jneda <julien.dane@gmail.com>
2022-12-27 10:36:17 +0100

    feat: add categories display in blog post view

--- view/partials/blogPost.php
+++ view/partials/blogPost.php
@@ -1,19 +1,33 @@
+<div class="row">
+  <!-- categories -->
+  <p class="fs-5 mt-3">
+    <?php foreach ($categories as $category) { ?>
+      <span class="badge bg-secondary me-auto p-2"><?= $category->getName() ?></span>
+    <?php } ?>
+  </p>
+</div>
+
 <div class="row my-3">
   <div class="col-lg-8 offset-lg-2">
+    <!-- post image -->
     <img src="<?php echo $post->getImage() ?>" class="img-fluid rounded w-100 d-block mx-auto" alt="<?php echo $post->getTitle() ?>">
+
+    <!-- post content -->
     <div class="my-3">
-      <h1><?php echo $post->getTitle() ?></h5>
-        <p>
-          par
-          <strong>
-            <a href="user.php?id=<?= $author->getUserId() ?>">
-              <?= $author->getUsername() ?>
-            </a>
-          </strong>
-          <span class="text-muted">- <?= $post->getFormattedDate() ?></span>
-        </p>
-        <p style="white-space: pre-wrap;"><?= $post->getContent() ?></p>
+      <h1><?php echo $post->getTitle() ?></h1>
+      <p>
+        par
+        <strong>
+          <a href="user.php?id=<?= $author->getUserId() ?>">
+            <?= $author->getUsername() ?>
+          </a>
+        </strong>
+        <span class="text-muted">- <?= $post->getFormattedDate() ?></span>
+      </p>
+      <p style="white-space: pre-wrap;"><?= $post->getContent() ?></p>
     </div>
+
+    <!-- user actions -->
     <a href="index.php" class="btn btn-outline-primary my-1">
       &laquo; Je retourne lire d'autres articles !
     </a>
