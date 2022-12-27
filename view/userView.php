559e6792557f2f2915fe0d53cbfb7350438c4212
jneda <julien.dane@gmail.com>
2022-12-27 09:51:05 +0100

    fix: use same layout as index page

--- view/userView.php
+++ view/userView.php
@@ -4,7 +4,7 @@ require_once './view/partials/header.php';
 
 ?>
 
-<div class="container my-3 col-md-6 offset-md-3">
+<div class="container my-3 col-lg-6 offset-lg-3">
   <h1>Articles écrits par <?= $user->getUsername() ?> :</h1>
   <?php require_once './view/partials/blogPostsList.php'; ?>
   <a href="index.php" class="btn btn-outline-primary">&laquo; Je retourne à l'accueil !</a>
