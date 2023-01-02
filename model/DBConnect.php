724b4fdb8139b7de1ec099f2b0f25c0faa274039
jdane <julien.dane@gmail.com>
2023-01-02 08:54:24 +0100

    fix: db password

--- model/DBConnect.php
+++ model/DBConnect.php
@@ -3,7 +3,7 @@
 function dbConnect(): PDO
 {
   try {
-    $databaseHandle = new PDO('mysql:host=localhost;dbname=tp_blog', 'root', 'root');
+    $databaseHandle = new PDO('mysql:host=localhost;dbname=tp_blog', 'root', '');
     $databaseHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $databaseHandle;
   } catch (PDOException $e) {
