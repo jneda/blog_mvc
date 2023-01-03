cf8af45da3aa4691a9f3ec939ce4ef8467618ac5
jdane <julien.dane@gmail.com>
2023-01-03 15:49:24 +0100

    backup: update database

--- backup/tp_blog.sql
+++ backup/tp_blog.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jan 02, 2023 at 04:49 PM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- Hôte : localhost:3306
+-- Généré le : mar. 03 jan. 2023 à 14:48
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tp_blog`
+-- Base de données : `tp_blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Structure de la table `category`
 --
 
 CREATE TABLE `category` (
@@ -33,15 +33,17 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `category`
+-- Déchargement des données de la table `category`
 --
 
 INSERT INTO `category` (`id_category`, `category_name`) VALUES
 (1, 'Anthony'),
 (8, 'Chocolatine'),
 (6, 'Cray-1'),
+(11, 'Dark Sasuke est un génie'),
 (3, 'Histoire de ma vie'),
 (2, 'Houlala'),
+(10, 'Le webmaster est super nul'),
 (4, 'Ordinateur'),
 (9, 'Pain au chocolat'),
 (7, 'Piratage informatique'),
@@ -50,7 +52,7 @@ INSERT INTO `category` (`id_category`, `category_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comment`
+-- Structure de la table `comment`
 --
 
 CREATE TABLE `comment` (
@@ -62,7 +64,7 @@ CREATE TABLE `comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `comment`
+-- Déchargement des données de la table `comment`
 --
 
 INSERT INTO `comment` (`id_comment`, `id_post`, `id_author`, `date`, `content`) VALUES
@@ -77,7 +79,7 @@ INSERT INTO `comment` (`id_comment`, `id_post`, `id_author`, `date`, `content`) 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Structure de la table `post`
 --
 
 CREATE TABLE `post` (
@@ -90,20 +92,21 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `post`
+-- Déchargement des données de la table `post`
 --
 
 INSERT INTO `post` (`id_post`, `title`, `content`, `date`, `id_author`, `image`) VALUES
-(1, 'Bienvenue sur mon blog ! 😁', 'Je m\'appelle Anthony Houlala et je suis développeur web.\r\nIci, vous pourrez suivre mes aventures et écrire des commentaires pour m\'encourager !\r\nÀ plus dans le bus !', '2022-12-21 14:35:43', 1, 'https://iili.io/HI1eCRs.jpg'),
-(2, 'Mon nouvel ordinateur ! 😍', 'Mon serveur personnel n\'arrivait plus à soutenir le trafic des visiteurs de mon blog.\r\nPlutôt que d\'optimiser l\'accès à la base de données, j\'en ai profité pour réaliser mon rêve : j\'ai acheté un Cray-1 !\r\nIl est pas beau mon bébé ? Dites-moi ce que vous en pensez en commentaire. 😉', '2022-12-21 14:38:24', 1, 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Cray-1_large_view.png/640px-Cray-1_large_view.png'),
-(3, 'J\'ai été piraté ! 😱', 'Je naviguais tranquillement sur le web, à la recherche de marqueurs effaçables performants et pas chers, quand tout à coup mon clavier a pris feu !\r\nDepuis, mon bébé refuse de démarrer. 😭\r\nHeureusement, je suis un professionnel : un formatage du disque dur devrait résoudre le problème.', '2022-12-21 14:50:57', 1, 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Burning_Logitech_12.jpg/640px-Burning_Logitech_12.jpg'),
-(7, 'Les chocolatines c&#039;est bon ! 🙌', 'Salut tout le monde, Anthony m&#039;a invité sur son super site pour y partager ma passion des CHOCOLATINES ! 😍\nÀ bientôt pour un article passionnant sur la fabrication de la pâte feuilletée. 😉', '2022-12-23 14:48:15', 7, 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Pain_au_chocolat_Luc_Viatour.jpg/640px-Pain_au_chocolat_Luc_Viatour.jpg'),
-(10, 'La pâte feuilletée c&#039;est dur ! 😬', 'La pâte feuilletée est réalisée à partir d&#039;une détrempe composée de farine, de sel et d&#039;eau. Des produits d&#039;addition peuvent être ajoutés à des fins technologiques ou pour optimiser la conservation du produit. Le type de mélange couramment réalisé est un pétrissage qui assure la répartition homogène et uniforme des constituants et l&#039;obtention d&#039;une masse. Des mouvements mécaniques de fraseurs dans une cuve mélange permettent une structuration de la pâte, son réseau de gluten est notamment formé.\n\nAprès un certain temps de repos, cette pâte est abaissée par le biais d&#039;un processus de laminage qui consiste en l&#039;utilisation de rouleaux en rotation en sens inverse qui exercent une pression sur celle-ci. Leur écartement est progressif afin d&#039;éviter d&#039;éventuels déchirements. L&#039;épaisseur de la pâte étant alors diminuée, on y étale alors de la matière grasse (généralement du beurre sec dit de tourage, mais parfois de la margarine ou de l&#039;huile), avant de replier la pâte, la pivoter et recommencer l&#039;opération plusieurs fois (classiquement six), en faisant reposer la pâte au réfrigérateur entre chaque tour, pour que la pâte perde son élasticité et que la matière grasse durcisse. C&#039;est cette opération dite de « tourage » (abaisser, tourner, plier) qui permet d&#039;obtenir un nombre important de couches, par rapport à la pâte « feuille à feuille » classique, donnant ainsi une pâte plus légère.\n\nLe nombre de feuilles de la pâte feuilletée f peut ainsi se déduire de f = b + 1 où b b, le nombre de couches de beurre, se calcule par la formule b = ( p + 1 ) n, p étant le nombre de plis effectués, et n le nombre de fois où la pâte a été pliée.\n\nAinsi, dans la recette classique, en pliant la pâte en trois (deux plis type pli roulé) et en réalisant six tours, on obtient b = ( 2 + 1 ) 6 soit 729 couches de beurre et donc 730 feuillets de pâte.\n\nLors de la cuisson, la chaleur du four engendre une évaporation de l’eau sous forme de chaleur dirigée vers le sommet de la pâte. Cette vapeur d&#039;eau exerce une pression sur les différentes couches de matière grasse qui sont imperméables. La rétention gazeuse provoquée permet le développement du feuilletage et ainsi l&#039;acquisition du volume final requis par gonflement des feuillets. À la fin du cycle de cuisson, la matière grasse est absorbée par la pâte et le caractère moelleux de la pâte est alors renforcé.', '2022-12-27 11:18:10', 7, 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/640px-Cute_dog.jpg');
+(1, 'Bienvenue sur mon blog ! 😁', 'Je m\'appelle Anthony Houlala et je suis développeur web.\r\nIci, vous pourrez suivre mes aventures et écrire des commentaires pour m\'encourager !\r\nÀ plus dans le bus !', '2022-12-21 14:35:43', 1, 'uploads/anthony_houlala.jpg'),
+(2, 'Mon nouvel ordinateur ! 😍', 'Mon serveur personnel n\'arrivait plus à soutenir le trafic des visiteurs de mon blog.\r\nPlutôt que d\'optimiser l\'accès à la base de données, j\'en ai profité pour réaliser mon rêve : j\'ai acheté un Cray-1 !\r\nIl est pas beau mon bébé ? Dites-moi ce que vous en pensez en commentaire. 😉', '2022-12-21 14:38:24', 1, 'uploads/Cray-1.png'),
+(3, 'J\'ai été piraté ! 😱', 'Je naviguais tranquillement sur le web, à la recherche de marqueurs effaçables performants et pas chers, quand tout à coup mon clavier a pris feu !\r\nDepuis, mon bébé refuse de démarrer. 😭\r\nHeureusement, je suis un professionnel : un formatage du disque dur devrait résoudre le problème.', '2022-12-21 14:50:57', 1, 'uploads/Burning_Logitech.jpg'),
+(7, 'Les chocolatines c&#039;est bon ! 🙌', 'Salut tout le monde, Anthony m&#039;a invité sur son super site pour y partager ma passion des CHOCOLATINES ! 😍\nÀ bientôt pour un article passionnant sur la fabrication de la pâte feuilletée. 😉', '2022-12-23 14:48:15', 7, 'uploads/chocolatine.jpg'),
+(10, 'La pâte feuilletée c&#039;est dur ! 😬', 'La pâte feuilletée est réalisée à partir d&#039;une détrempe composée de farine, de sel et d&#039;eau. Des produits d&#039;addition peuvent être ajoutés à des fins technologiques ou pour optimiser la conservation du produit. Le type de mélange couramment réalisé est un pétrissage qui assure la répartition homogène et uniforme des constituants et l&#039;obtention d&#039;une masse. Des mouvements mécaniques de fraseurs dans une cuve mélange permettent une structuration de la pâte, son réseau de gluten est notamment formé.\n\nAprès un certain temps de repos, cette pâte est abaissée par le biais d&#039;un processus de laminage qui consiste en l&#039;utilisation de rouleaux en rotation en sens inverse qui exercent une pression sur celle-ci. Leur écartement est progressif afin d&#039;éviter d&#039;éventuels déchirements. L&#039;épaisseur de la pâte étant alors diminuée, on y étale alors de la matière grasse (généralement du beurre sec dit de tourage, mais parfois de la margarine ou de l&#039;huile), avant de replier la pâte, la pivoter et recommencer l&#039;opération plusieurs fois (classiquement six), en faisant reposer la pâte au réfrigérateur entre chaque tour, pour que la pâte perde son élasticité et que la matière grasse durcisse. C&#039;est cette opération dite de « tourage » (abaisser, tourner, plier) qui permet d&#039;obtenir un nombre important de couches, par rapport à la pâte « feuille à feuille » classique, donnant ainsi une pâte plus légère.\n\nLe nombre de feuilles de la pâte feuilletée f peut ainsi se déduire de f = b + 1 où b b, le nombre de couches de beurre, se calcule par la formule b = ( p + 1 ) n, p étant le nombre de plis effectués, et n le nombre de fois où la pâte a été pliée.\n\nAinsi, dans la recette classique, en pliant la pâte en trois (deux plis type pli roulé) et en réalisant six tours, on obtient b = ( 2 + 1 ) 6 soit 729 couches de beurre et donc 730 feuillets de pâte.\n\nLors de la cuisson, la chaleur du four engendre une évaporation de l’eau sous forme de chaleur dirigée vers le sommet de la pâte. Cette vapeur d&#039;eau exerce une pression sur les différentes couches de matière grasse qui sont imperméables. La rétention gazeuse provoquée permet le développement du feuilletage et ainsi l&#039;acquisition du volume final requis par gonflement des feuillets. À la fin du cycle de cuisson, la matière grasse est absorbée par la pâte et le caractère moelleux de la pâte est alors renforcé.', '2022-12-27 11:18:10', 7, 'uploads/Cute_dog.jpg'),
+(18, 'Ce site est super nul 😪', 'Va toucher l&#039;herbe Anthony !', '2023-01-03 12:56:07', 12, 'uploads/095-pile-of-poo.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post_category`
+-- Structure de la table `post_category`
 --
 
 CREATE TABLE `post_category` (
@@ -113,7 +116,7 @@ CREATE TABLE `post_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `post_category`
+-- Déchargement des données de la table `post_category`
 --
 
 INSERT INTO `post_category` (`id_post_category`, `id_post`, `id_category`) VALUES
@@ -136,12 +139,14 @@ INSERT INTO `post_category` (`id_post_category`, `id_post`, `id_category`) VALUE
 (17, 7, 5),
 (21, 10, 8),
 (22, 10, 9),
-(23, 10, 5);
+(23, 10, 5),
+(29, 18, 11),
+(30, 18, 10);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
 CREATE TABLE `user` (
@@ -152,7 +157,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `password`) VALUES
@@ -164,18 +169,18 @@ INSERT INTO `user` (`id_user`, `username`, `email`, `password`) VALUES
 (13, 'vengeuse_masquee', 'maria@db.org', '$2y$10$GAjW49R1mCKuMX170IVDpOiwtMh9WyR4Er3eQbLT8MAv20nEBy5im');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `category`
+-- Index pour la table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id_category`),
   ADD KEY `category_name` (`category_name`);
 
 --
--- Indexes for table `comment`
+-- Index pour la table `comment`
 --
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`id_comment`),
@@ -183,14 +188,14 @@ ALTER TABLE `comment`
   ADD KEY `id_author` (`id_author`);
 
 --
--- Indexes for table `post`
+-- Index pour la table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`id_post`),
   ADD KEY `id_author` (`id_author`);
 
 --
--- Indexes for table `post_category`
+-- Index pour la table `post_category`
 --
 ALTER TABLE `post_category`
   ADD PRIMARY KEY (`id_post_category`),
@@ -198,64 +203,64 @@ ALTER TABLE `post_category`
   ADD KEY `id_category` (`id_category`);
 
 --
--- Indexes for table `user`
+-- Index pour la table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_category` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `comment`
+-- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
   MODIFY `id_comment` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `post`
+-- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id_post` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_post` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `post_category`
+-- AUTO_INCREMENT pour la table `post_category`
 --
 ALTER TABLE `post_category`
-  MODIFY `id_post_category` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_post_category` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `comment`
+-- Contraintes pour la table `comment`
 --
 ALTER TABLE `comment`
   ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Constraints for table `post`
+-- Contraintes pour la table `post`
 --
 ALTER TABLE `post`
   ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
--- Constraints for table `post_category`
+-- Contraintes pour la table `post_category`
 --
 ALTER TABLE `post_category`
   ADD CONSTRAINT `post_category_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`) ON DELETE RESTRICT ON UPDATE RESTRICT,
