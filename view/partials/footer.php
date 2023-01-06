5f1a437ef1048e3c0089148bdf5eb58bfdb2881c
jneda <julien.dane@gmail.com>
2023-01-06 11:21:20 +0100

    feat: add footer

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,3 +1,10 @@
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <hr>
+    <span class="text-muted">&copy; Anthony Houlala</span>
+  </div>
+</footer>
+
 </body>
 
 </html>
