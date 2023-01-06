2b6102dab70fe38c374423df590a5d4ea71247e1
jneda <julien.dane@gmail.com>
2023-01-06 11:17:22 +0100

    fix: limit image height

--- view/partials/blogpostCard.php
+++ view/partials/blogpostCard.php
@@ -1,6 +1,6 @@
 <div class="col-md-6">
-  <div class="card my-3 shadow-sm">
-    <img src="uploads/<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>" style="max-height: 20rem; object-fit: cover;">
+  <div class="card my-3 shadow-sm h-100">
+    <img src="uploads/<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>" style="max-height: 10rem; object-fit: cover;">
     <div class="card-body d-flex flex-column justify-content-between">
       <h2 class="card-title my-3"><?php echo $post->getTitle() ?></h2>
       <!-- <p class="card-text"><?php echo $post->getContent() ?></p> -->
