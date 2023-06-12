6f4693e7e5b09c5cca3cc01bd67e9dcbb8ad5049
Julien Dané <julien.dane@gmail.com>
2023-06-12 13:28:34 +0200

    make login and signup inputs required

--- view/loginView.php
+++ view/loginView.php
@@ -8,11 +8,11 @@ require_once 'partials/navbar.php';
 <form class="container my-3" action="login.php" method="POST">
   <div class="mb-3 col-md-6 offset-md-3">
     <label for="email" class="form-label">Ton email :</label>
-    <input type="email" class="form-control" id="email" name="email">
+    <input type="email" class="form-control" id="email" name="email" required>
   </div>
   <div class="mb-3 col-md-6 offset-md-3">
     <label for="password" class="form-label">Ton mot de passe :</label>
-    <input type="password" class="form-control" id="password" name="password">
+    <input type="password" class="form-control" id="password" name="password" required>
   </div>
   <button type="submit" class="btn btn-primary offset-md-3">Se connecter</button>
   <a href="signup.php" class="btn btn-outline-primary">S'inscrire</a>
--- view/signupView.php
+++ view/signupView.php
@@ -8,15 +8,15 @@ require_once './view/partials/navbar.php';
 <form class="container my-3" action="signup.php" method="POST">
   <div class="mb-3 col-md-6 offset-md-3">
     <label for="username" class="form-label">Ton pseudo :</label>
-    <input type="text" class="form-control" id="username" name="username">
+    <input type="text" class="form-control" id="username" name="username" required>
   </div>
   <div class="mb-3 col-md-6 offset-md-3">
     <label for="email" class="form-label">Ton email :</label>
-    <input type="email" class="form-control" id="email" name="email">
+    <input type="email" class="form-control" id="email" name="email" required>
   </div>
   <div class="mb-3 col-md-6 offset-md-3">
     <label for="password" class="form-label">Ton mot de passe :</label>
-    <input type="password" class="form-control" id="password" name="password">
+    <input type="password" class="form-control" id="password" name="password" required>
   </div>
   <button type="submit" class="btn btn-primary offset-md-3">S'inscrire</button>
 </form>
