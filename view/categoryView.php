f16078e11124154a2d6c291f3844df2a0ad3afd2
Julien Dané <julien.dane@gmail.com>
2023-06-14 09:33:50 +0200

    styles

--- view/categoryView.php
+++ view/categoryView.php
@@ -7,7 +7,9 @@ require_once './view/partials/navbar.php';
 
 <div class="container my-3 col-lg-6 offset-lg-3">
   <h1>Articles pour la catégorie <?= $category->getName() ?> :</h1>
-  <?php require_once './view/partials/blogPostsList.php'; ?>
+  <div class="d-flex flex-wrap justify-content-center">
+    <?php require_once './view/partials/blogPostsList.php'; ?>
+  </div>
   <a href="index.php" class="btn btn-outline-primary my-3">&laquo; Je retourne à l'accueil !</a>
 </div>
 
--- view/partials/blogpostCard.php
+++ view/partials/blogpostCard.php
@@ -1,5 +1,5 @@
-<div class="col-md-6 mb-3">
-  <div class="card my-3 shadow-sm h-100">
+<div class="flex-fill m-2">
+  <div class="card shadow-sm h-100">
     <img src="uploads/<?php echo $post->getImage() ?>" class="card-img-top" alt="<?php echo $post->getTitle() ?>" style="max-height: 10rem; object-fit: cover;">
     <div class="card-body d-flex flex-column justify-content-between">
       <h2 class="card-title my-3"><?php echo $post->getTitle() ?></h2>
